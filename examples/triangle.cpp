1259d648c7542fbaab99d2b09a6b521f184f34ad
Andrew Ames <andyames@users.noreply.github.com>
2017-06-26 08:37:14 -0700

    Mac OS X triangle example works.

--- examples/triangle.cpp
+++ examples/triangle.cpp
@@ -31,7 +31,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // required on OS X
 
     // glfw window creation
     // --------------------
